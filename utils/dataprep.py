6be0c92b25acba31a568bbcae40e6bde3402ecbe
Haruto Kaito <kaiharuro.dev@gmail.com>
2019-09-04 22:27:27 +0200

    Starting Part 2

--- utils/dataprep.py
+++
@@ -1,9 +0,0 @@
-### Helper Functions for data preprocessing
-# Kai Haruto 2019/09/03
-
-import os
-#prepares images in folder for import
-#@in:path, labeldict as dictionairy
-#@out:None
-def sort_datafolders(path,
-filenamedict):
