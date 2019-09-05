c9d2ba0f28afda7923998dda738795d0627e835b
Haruto Kaito <kaiharuro.dev@gmail.com>
2019-09-05 11:58:05 +0200

    Finally finished model import in train.py

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ from torchvision import datasets, transforms, models
 from PIL import Image
 import numpy as np
 import json
+import torch.utils.model_zoo as model_zoo
+
 
 #functions -migrate eventually
 def train_model(model,optimizer,criterion,dataloader,n_classes, n_hidden, drop_p,
@@ -145,6 +147,36 @@ arch_dict = {
     'densenet' : models.densenet161(pretrained=False),
     'inception' : models.inception_v3(pretrained=False)
 }
+#URLS for statedicts
+#From https://github.com/Cadene/pretrained-models.pytorch/blob/master/pretrainedmodels/models/torchvision_models.py
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+    'densenet121': 'http://data.lip6.fr/cadene/pretrainedmodels/densenet121-fbdb23505.pth',
+    'densenet169': 'http://data.lip6.fr/cadene/pretrainedmodels/densenet169-f470b90a4.pth',
+    'densenet201': 'http://data.lip6.fr/cadene/pretrainedmodels/densenet201-5750cbb1e.pth',
+    'densenet161': 'http://data.lip6.fr/cadene/pretrainedmodels/densenet161-347e6b360.pth',
+    'inceptionv3': 'https://download.pytorch.org/models/inception_v3_google-1a9a5a14.pth',
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+    'squeezenet1_0': 'https://download.pytorch.org/models/squeezenet1_0-a815701f.pth',
+    'squeezenet1_1': 'https://download.pytorch.org/models/squeezenet1_1-f364aa15.pth',
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+    'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+    'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+    # 'vgg16_caffe': 'https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg16-00b39a1b.pth',
+    # 'vgg19_caffe': 'https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg19-d01eb7cb.pth'
+}
+
+
+print('Preparing Pretrained models..')
 
 
 
@@ -245,6 +277,7 @@ dataloaders = {'train' : torch.utils.data.DataLoader(image_datasets['train'],
 
 #get model
 model = arch_dict[arch]
+model.load_state_dict(model_zoo.load_url(model_urls[arch]))
 
 # Freeze model params
 for param in model.parameters():
