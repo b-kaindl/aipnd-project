b80996bfbbdd97daf9ca91191889998dc94719d2
Haruto Kaito <kaiharuro.dev@gmail.com>
2019-09-05 02:52:07 +0200

    Added Full Training Routine to train.py

--- train.py
+++ train.py
@@ -3,13 +3,60 @@ import argparse
 import os
 from collections import OrderedDict
 import torch
+from torch import nn, optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 from PIL import Image
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 import json
+
+#functions -migrate eventually
+def train_model(model,optimizer,criterion,dataloader,n_classes, n_hidden, drop_p,
+learn_rate,cat_to_name, n_epochs=1,
+device='cpu',save_dir =''):
+    print('Start Training')
+    running_loss = 0
+    steps = 0
+    print_every = 20
+    train_size = len(dataloader)
+    model.train()
+    for epoch in range(n_epochs):
+        for images, flowers in dataloader:
+            images, flowers = images.to(device), flowers.to(device)
+
+            optimizer.zero_grad()
+
+            logps = model.forward(images)
+            loss = criterion(logps, flowers)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            steps += 1
+
+            if steps % print_every == 0:
+                    print(f"Epoch {epoch+1}/{n_epochs}.."
+                      f"Train Loss: {running_loss/steps:.3f}..")
+
+
+    model.class_to_idx =cat_to_name
+    checkpoint = {
+    'input_size' : model.classifier[0].in_features,
+    'output_size' : n_classes,
+    'hidden_size' : n_hidden,
+    'state_dict' : model.classifier.state_dict(),
+    'epochs': n_epochs,
+    'learn_rate': learn_rate,
+    'optimizer' : optimizer,
+    'optim_state': optimizer.state_dict(),
+    'dropout_prob': drop_p,
+    'criterion' : loss,
+    'class_to_idx': model.class_to_idx
+    }
+
+
+
+    torch.save(checkpoint, save_dir + 'checkpoint.pth')
 
 
 #define user options here
@@ -25,11 +72,15 @@ option_dict = {
     '--epochs' : 'def. 20',
     '--gpu' : 'Enable GPU',
     '--mapping_file' : '.json mapping ID:Name', #for prediction
-    '--arch' : 'pretrained architecture, def. \'vgg13\''
+    '--arch' : 'pretrained architecture, def. \'vgg16\''
 }
 
 parser.add_argument("data_dir",
 help=option_dict['data_dir'],
+type = str)
+
+parser.add_argument("--save_dir",
+help=option_dict['--save_dir'],
 type = str)
 
 parser.add_argument('--learning_rate',
@@ -49,8 +100,8 @@ parser.add_argument("--epochs",
 help=option_dict['--epochs'],
 type = int)
 
-# parser.add_argument("--mapping_file",
-# help=option_dict['--mapping_file'], type =str)
+parser.add_argument("--mapping_file",
+help=option_dict['--mapping_file'], type =str)
 args = parser.parse_args()
 
 
@@ -68,6 +119,7 @@ arch_dict = {
     'densenet' : models.densenet161(pretrained=False),
     'inception' : models.inception_v3(pretrained=False)
 }
+
 
 
 #TODO IF
@@ -91,17 +143,22 @@ train_dir = data_dir + '/train'
 valid_dir = data_dir + '/valid'
 test_dir = data_dir + '/test'
 
-print(data_dir)
-
+if args.mapping_file:
+    with open(args.mapping_file, 'r') as f:
+        cat_to_name = args.mapping_file
+else:
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
 if args.hidden_units:
     hidden_units =args.hidden_units #--hidden_units
 else:
     hidden_units =512 #--hidden_units
 
-if args.epoch:
-    n_epochs =args.epoch
+if args.epochs:
+    n_epochs =args.epochs
 else:
-    n_epochs =20
+    n_epochs =5
+
 batch_size = 32
 
 #to predictiondir
@@ -118,7 +175,12 @@ else:
 if args.arch:
     arch =args.arch
 else:
-    arch = 'vgg13'
+    arch = 'vgg16'
+
+if args.save_dir:
+    save_dir = args.save_dir
+else:
+    save_dir=''
 
 
 data_transforms = {
@@ -148,20 +210,44 @@ image_datasets = {'train' : datasets.ImageFolder(train_dir, data_transforms['tra
 
 # Using the image datasets and the trainforms, define the dataloaders
 dataloaders = {'train' : torch.utils.data.DataLoader(image_datasets['train'],
-                                                     batch_size = 32,
+                                                     batch_size = batch_size,
                                                      shuffle=True),
                'test' : torch.utils.data.DataLoader(image_datasets['test'],
-                                                     batch_size = 32,
+                                                     batch_size = batch_size,
                                                      shuffle=True),
                'valid': torch.utils.data.DataLoader(image_datasets['valid'],
-                                                     batch_size = 32,
+                                                     batch_size = batch_size,
                                                      shuffle=True)}
 
 #get model
 model = arch_dict[arch]
+
 # Freeze model params
 for param in model.parameters():
     param.requires_grad = False
 
-#model
-#define default initialized model.
+drop_p =.2
+n_inputs = model.classifier[0].in_features
+n_hidden =hidden_units
+n_classes =102
+
+flower_classifier = nn.Sequential(OrderedDict([
+    ('fc1', nn.Linear(n_inputs,n_hidden)),
+    ('relu1', nn.ReLU()),
+    ('fc2', nn.Linear(n_hidden,n_classes)),
+    ('dropout', nn.Dropout(drop_p)),
+    ('logsoftmax', nn.LogSoftmax(dim=1))
+]))
+
+model.classifier = flower_classifier
+
+#loss function, opimizer
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.classifier.parameters(), lr = learning_rate)
+
+model.to(device)
+
+train_model(model,optimizer,criterion,dataloaders['train'],
+n_classes = n_classes, n_hidden=n_hidden, drop_p=drop_p,
+learn_rate=learning_rate,cat_to_name = cat_to_name, n_epochs=n_epochs,
+device = device ,save_dir = save_dir)
